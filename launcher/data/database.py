b0eaebfe0e075fe7593975455d7e6bcd061711cc
zhujiu0 <1904272790@qq.com>
2026-09-21 16:00:39 +0800

    Add database.py with game URLs and configurations

---
+++ launcher/data/database.py
@@ -0,0 +1,147 @@
+DRIVER_PATH = r"D:\chrome\chromedriver-win64\chromedriver-win64\chromedriver.exe"
+# game_test_url
+url_gametest = "https://game_test.test928.xyz/#/start"
+# game demo test
+url_gamedemo = 'https://game100.org/#/home'
+# game_test4
+url_gametest4 = 'https://test4.bpapiglobal.com/'
+# gin_vue_admin
+url_ginvueadmin = "https://admin_l_ziyan.1b12.pro/#/login?redirect=%23/layout/admin/api"
+username = "admin_awen"
+password = "123456"
+verification_code = "slot_v2撒娇的年卡是否_奥斯曼dams发卡吗_阿斯弗"
+#heibao
+url_heibao = 'https://open_admin.test928.xyz/#/login?redirect=%23/layout/dashboard'
+username_heibao = 'gongsiceshi_awen'
+password_heibao = 'Aa123456789'
+#SLOAT_GAME
+url_slote = "https://admin_bossqian.1b12.pro/"
+username_slote = 'awen_playngo'
+password_slote = 'nihao123456'
+verification_slote = "A-slot123456ijop&**(*&^!$$"
+#bg
+url_bgslote = 'https://admin_bg.1b12.pro/#/layout/adapter'
+username_bgslote = 'admin_awen'
+
+
+'''
+bg
+admin：
+https://admin_bg.1b12.pro/#/layout/adapter
+username: admin_awen
+password: admin_awen-dklzfgfg
+A-slot123456ijop&**(*&^!$$
+
+https://bgaming.com/
+
+heibao
+admin_zero
+123456
+
+
+
+pp
+admin_awen
+lourseam
+!cnmw.asdas@#$qwe789./,槜
+https://admin_ppneo.1b12.pro/#
+
+
+yuanjun:tp
+地址 1：http://admin_yuyue.1b1.pro/
+账号 admin_awen
+密码 nihao123456
+验证码 qwuih23hgb3uh3d91h9j+-21001
+
+地址 2：https://adminyy_yj.1b12.pro/
+账号 admin_awen
+密码 nihao123456
+验证码 12138slidfj39urlsdj89fv943ut0njklvnio
+
+正常调数值、抽样本，都可以在地址 2 搞。如果要重启的话，只能用地址 1
+'''
+# country
+data_list = [
+    {"rich_code": "BRL", "region": "1", "language": "pt", "Country": "巴西x4"
+        , "apihost": "https://brapi.bpapiglobal.com/"},
+    {"rich_code": "THB", "region": "2", "language": "th", "Country": "泰国x8"
+        , "apihost": "https://thapi.bpapiglobal.com/"},
+    {"rich_code": "BDT", "region": "4", "language": "en", "Country": "孟加拉x10"
+        , "apihost": "https://bdtapi.bpapiglobal.com/"},
+    {"rich_code": "PKR", "region": "5", "language": "en", "Country": "MUMAPIx40"
+        , "apihost": "https://mumbaiapi.bpapiglobal.com/"},
+    {"rich_code": "PHP", "region": "6", "language": "en", "Country": "菲律宾x6"
+        , "apihost": "https://phpapi.bpapiglobal.com/"},
+    {"rich_code": "VNDK", "region": "7", "language": "vi", "Country": "越南x5"
+        , "apihost": "https://vndapi.bpapiglobal.com/"},
+    {"rich_code": "MYR", "region": "8", "language": "en", "Country": "马来西亚x2"
+        , "apihost": "https://myrapi.bpapiglobal.com/"},
+    {"rich_code": "IDRK", "region": "9", "language": "id", "Country": "印尼x1"
+        , "apihost": "https://idrapi.bpapiglobal.com/"},
+    {"rich_code": "INR", "region": "10", "language": "en", "Country": "印度x10"
+        , "apihost": "https://inrapi.bpapiglobal.com/"},
+    {"rich_code": "TRY", "region": "11", "language": "tr", "Country": "土耳其x5"
+        , "apihost": "https://tryapi.bpapiglobal.com/"},
+    {"rich_code": "LAKK", "region": "12", "language": "en", "Country": "老挝x1"
+        , "apihost": "https://lakapi.bpapiglobal.com/"},
+    {"rich_code": "AUD", "region": "13", "language": "en", "Country": "澳大利亚x1"
+        , "apihost": "https://audapi.bpapiglobal.com/"},
+    {"rich_code": "MMK", "region": "14", "language": "en", "Country": "缅甸x200"
+        , "apihost": "https://mmkapi.bpapiglobal.com/"},
+    {"rich_code": "MXN", "region": "15", "language": "es", "Country": "墨西哥x4"
+        , "apihost": "https://midameapi.bpapiglobal.com/"},
+    {"rich_code": "ZAR", "region": "16", "language": "en", "Country": "南非x1"
+        , "apihost": "https://zarapi.bpapiglobal.com/"},
+    {"rich_code": "JPY", "region": "17", "language": "ja", "Country": "日本x10"
+        , "apihost": "https://jpyapi.bpapiglobal.com/"},
+    {"rich_code": "KRW", "region": "18", "language": "ko", "Country": "韩国x100"
+        , "apihost": "https://krwapi.bpapiglobal.com/"},
+    {"rich_code": "TWD", "region": "19", "language": "en", "Country": "台湾x8"
+        , "apihost": "https://twdapi.bpapiglobal.com/"},
+    {"rich_code": "XOF", "region": "40000", "language": "en", "Country": "西非法郎x100"
+        , "apihost": "https://xofapi.bpapiglobal.com/"},
+{"rich_code": "PKR", "region": "11500", "language": "en", "Country": "巴基斯坦x40"
+        , "apihost": "https://pkrapi.bpapiglobal.com/"},
+]
+
+# game_test 胜率选择
+xpath_map = {
+    "9650": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[1]',
+    "9600": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[2]',
+    "9550": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[3]',
+    "9500": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[4]',
+    "9400": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[5]',
+    "98.5": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[6]',
+    "100": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[7]',
+    "200": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[8]',
+    "10095": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[9]',
+    "10096": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[10]',
+    "10097": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[11]',
+    "10098": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[12]',
+    "10099": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[2]/div[1]/ul/li[13]',
+}
+
+XPATH = {
+
+    "wallet_test4": '//*[@id="app"]/div/div/form/div[1]/div[19]/div/div[2]',
+    "wallet":       '//*[@id="app"]/div/div/form/div[1]/div[19]/div/div[1]',
+
+    "id_input": "//input[@placeholder='ID']",
+    "key_input": "//input[@placeholder='KEY']",
+    "apiHost_input": "//label[contains(text(),'apiHost')]/following::input[1]",
+    "apihost_test4": "",
+    "username_input": "//input[@placeholder='玩家用户名，可以使用使用字母和数字以及下划线']",
+    "currency_input": "//label[contains(text(),'货币')]/following::input[1]",
+    "region_input": "//label[contains(text(),'地区')]/following::input[1]",
+    "language_input": "//label[contains(text(),'语言')]/following::input[1]",
+    "win_rate_dropdown": "//label[contains(text(),'胜率')]/following::input[1]",
+    "win_rate_confirm": '//*[@id="app"]/div/div/form/div[1]/div[11]/div/div[1]/button[2]',
+    "balance_input": "//label[contains(text(),'余额')]/following::input[1]",
+    "login_btn": '//*[@id="app"]/div/div/form/div[2]/button',
+    # 平台Xpath
+    "fc_fish_card": '//*[@id="app"]/div/div/div[3]/div[1]/div/div[48]/div',
+    # 游戏Xpath
+    "enter_game": '//*[@id="app"]/div/div/div[3]/div[2]/div[3]/div/div[123]/div/div',
+
+    "menu_btn":'//*[@id="menuButtonWrapper-hit-area"]'
+}
